14d5d421b122469c7f6ed6e50c5372867d472716
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-05-11 21:33:34 +0530

    Employee is present or not

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,23 @@
+﻿﻿namespace employeeWageComputation
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        const int Is_Present = 1;
+        Random random = new Random();
+        int randomInput = random.Next(0, 2);
+        if (Is_Present == randomInput)
+        {
+            Console.WriteLine("employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("employee is absent");
+        }
+    }
+
+}
+
+}
