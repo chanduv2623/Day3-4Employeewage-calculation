f6ce430cae1d4a83124ef3adedb83ffdd176e8ee
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-05-30 19:37:52 +0530

    EmployeeWageCalculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,20 +4,26 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        const int Is_Present = 1;
-        Random random = new Random();
-        int randomInput = random.Next(0, 2);
-        if (Is_Present == randomInput)
-        {
-            Console.WriteLine("employee is Present");
-        }
-        else
-        {
-            Console.WriteLine("employee is absent");
-        }
-    }
+            Console.WriteLine("welcomes to the employeeWage calculations !");
+            const int Full_Time = 1;
+            int empWage = 0;
+            const int Rate_Per_Hour = 20;
+            int empHrs = 0;
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (Full_Time == randomInput)
+            {
+                empHrs = 8;
+                Console.WriteLine("employee is Present");
+                           }
+            else
+            {
+                Console.WriteLine("employee is absent");
 
-}
+            }
+            empWage = empHrs * Rate_Per_Hour;
+            Console.WriteLine(empWage);
+        }
+        }
 
 }
