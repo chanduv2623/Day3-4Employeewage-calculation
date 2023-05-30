3f7a48ad95cabe35847ad98229886b1442d5fa58
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-05-30 20:10:17 +0530

    PartTimeEmployee&Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,24 +4,47 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            Console.WriteLine("Welcome to Employee Wage Calculation");
+
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+
+            int empWage = 0;
+
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
+            int empCheck = random.Next(0, 3);
 
+            if (empCheck == IS_PART_TIME)
+
+            {
                 Console.WriteLine("Employee is Present");
+                empHrs = 4;
+
+            }
+
+            else if (empCheck == IS_FULL_TIME)
+
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
 
             }
 
             else
 
             {
-
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
 
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR; Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
